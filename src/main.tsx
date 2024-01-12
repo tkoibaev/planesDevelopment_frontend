341a31ee6148692473cd6778ba8392c14adcc4a5
tkoibaev <tam.koib12@gmail.com>
2024-01-12 22:52:23 +0300

    add hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import { HashRouter } from "react-router-dom"
+import "./index.css"
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
-);
+  </HashRouter>
+)
