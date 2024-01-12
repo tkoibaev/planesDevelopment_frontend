3374a2594233eaf74c1e69cbf1e8ec6f953916e1
tkoibaev <tam.koib12@gmail.com>
2024-01-12 22:57:39 +0300

    add hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import "./App.css";
-import { Navigate, Route, Routes } from "react-router-dom";
-import Header from "./components/Header/Header";
-import MainPage from "./pages/MainPage/MainPage";
-import PlanePage from "./pages/PlanePage/PlanePage";
-import Breadcrumps from "./components/Breadcrumps/Breadcrumps";
-import RegPage from "./pages/RegPage/RegPage";
-import AuthPage from "./pages/AuthPage/AuthPage";
+import "./App.css"
+import { Navigate, Route, Routes } from "react-router-dom"
+import Header from "./components/Header/Header"
+import MainPage from "./pages/MainPage/MainPage"
+import PlanePage from "./pages/PlanePage/PlanePage"
+import Breadcrumps from "./components/Breadcrumps/Breadcrumps"
+import RegPage from "./pages/RegPage/RegPage"
+import AuthPage from "./pages/AuthPage/AuthPage"
 
 function App() {
   return (
@@ -13,10 +13,7 @@ function App() {
       <Header />
       <Breadcrumps />
       <Routes>
-        <Route
-          path="/"
-          element={<Navigate to="/planesDevelopment_frontend" replace />}
-        />
+        <Route path="/" element={<MainPage />} />
         <Route path="/planesDevelopment_frontend" element={<MainPage />} />
         {/* <Route path="/planes" element={<PlanePage />} /> */}
         <Route path="/planesDevelopment_frontend/:id" element={<PlanePage />} />
@@ -27,7 +24,7 @@ function App() {
         <Route path="/planesDevelopment_frontend/auth" element={<AuthPage />} />
       </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
