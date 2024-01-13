6a4b5e295da5e6bd9b2ccacda326d5f725b9817e
tkoibaev <tam.koib12@gmail.com>
2024-01-13 23:05:41 +0300

    pages

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css"
 import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 )
